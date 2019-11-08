3993e2b7b57cc865fbe9b421995e8b59ec8e2153
nathanah <nathanahoffman@gmail.com>
2019-11-08 12:08:23 -0800

    grid search

--- ann.py
+++ ann.py
@@ -13,8 +13,9 @@ from keras import optimizers
 
 
 #import data
-file = "processed.data" #?????????????????
-names = ["word_freq_make",
+file = "spambase.data"
+names = ["spamClassification",
+        "word_freq_make",
         "word_freq_address",
         "word_freq_all",
         "word_freq_3d",
@@ -72,38 +73,51 @@ names = ["word_freq_make",
         "capital_run_length_longest",
         "capital_run_length_total",
         ]
-data = pd.read_csv(file, delim=",", names=names)
+data = pd.read_csv(file, delimiter=",", names=names)
 
-
+x = data.iloc[:,1:]
+y = data.iloc[:,0]
 
 
 
 
 #grid search
-trainingAccuracy = np.zeros((3,4))
-testingAccuracy = np.zeros((3,4))
+maxLayers = 3
 gridNodes = [3,6,9,12]
-for layers in range(1,4):
+trainingAccuracy = np.zeros((maxLayers,len(gridNodes)))
+testingAccuracy = np.zeros((maxLayers,len(gridNodes)))
+
+for layers in range(1,maxLayers+1):
     for nodes in range(0,len(gridNodes)):
         #set up ann
         ann = Sequential()
         ann.add(Dense(units=gridNodes[nodes], input_shape=(57,), activation="relu")) #hidden layers
         for l in range(1,layers):
             ann.add(Dense(units=gridNodes[nodes], activation="relu"))
-        ann.add(Dense(units=1, activation="softmax")) #output layer
+        ann.add(Dense(units=1, activation="sigmoid")) #output layer
 
 
         earlyStop = keras.callbacks.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=100, mode='min', baseline=None, restore_best_weights=False)
 
         sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=False)
-        ann.compile(loss="categorical_crossentropy",
+        ann.compile(loss="mean_squared_error",
                     optimizer=sgd,
                     metrics=["accuracy"])
 
 
-        info = ann.fit(x,yCat,
+        info = ann.fit(x,y,
                         validation_split=.34,
                         shuffle=False,
-                        epochs=10000,
+                        epochs=10,
                         batch_size=32,
                         callbacks=[earlyStop])
+        trainingAccuracy[layers-1][nodes] = info.history['accuracy'][len(info.history)-1]
+        testingAccuracy[layers-1][nodes] = info.history['val_accuracy'][len(info.history)-1]
+
+
+
+
+print("Training Accuracy")
+print(trainingAccuracy)
+print("Testing Accuracy")
+print(testingAccuracy)
