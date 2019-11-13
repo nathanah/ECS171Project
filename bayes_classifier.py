23e0e0113a5d287dfe13a6338f7172a0d0ed7bce
agentjsp <38056528+agentjsp@users.noreply.github.com>
2019-11-13 14:49:33 -0800

    Begin classify method, and add utility method

--- bayes_classifier.py
+++ bayes_classifier.py
@@ -1,4 +1,5 @@
 import nltk
+import math
 
 def load_data(model, class1_tokens, class2_tokens, test):
 	file = open('SMSSpamCollection', 'r', encoding='utf_8')
@@ -18,8 +19,21 @@ def load_data(model, class1_tokens, class2_tokens, test):
 	model['class1_fd'] = nltk.FreqDist(class1_tokens)
 	model['class2_fd'] = nltk.FreqDist(class2_tokens)
 
+def classify(model):
+	class1_tokens, class2_tokens = model['class1_fd'], model['class2_fd']
+
+	prior_class1 = return_prior(model, 'class1_count')
+	prior_class2 = return_prior(model, 'class2_count')
+
+	print(prior_class1, prior_class2)
+
+def return_prior(model, class_name):
+	return math.log(model[class_name]/ \
+		(model['class1_count'] + model['class2_count']))
+
 if __name__ == '__main__':
 	model = {}
 	class1_tokens, class2_tokens = [], []
 	test = []
 	load_data(model, class1_tokens, class2_tokens, test)
+	classify(model)
