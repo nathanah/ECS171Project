af6fc3aceac8da1e57c8100cf6c1f6eff7c6d4b6
nathanah <nathanahoffman@gmail.com>
2019-11-24 17:11:11 -0800

    [ANN] made testing easier?

--- ANN_draft.py
+++ ANN_draft.py
@@ -15,8 +15,8 @@ from sklearn.metrics import auc
 
 
 def error_plot(X, y, model, numEpochs = 1000):
-    
-    
+
+
     #callback to collect testing and training accuracy at end of each epoch
     class accuracyHistory(callbacks.Callback):
         def on_train_begin(self, logs={}):
@@ -25,11 +25,11 @@ def error_plot(X, y, model, numEpochs = 1000):
         def on_epoch_end(self, epoch, logs={}):
             self.accuracies.append(logs.get('binary_accuracy'))
             self.val_accuracies.append(logs.get('val_binary_accuracy'))
-            
+
     acc_Callback = accuracyHistory()
     acc_matrix = np.zeros([10, numEpochs])
     val_acc_matrix = np.zeros([10, numEpochs])
-    
+
     kf = StratifiedKFold(n_splits=10)
     idx = 0
     for train_index, test_index in kf.split(X = X, y = y):
@@ -38,24 +38,24 @@ def error_plot(X, y, model, numEpochs = 1000):
         training_y = np.asarray(y)[train_index]
         testing_y = np.asarray(y)[test_index]
         #Fit model with callback
-        info = model.fit(training_x, training_y, batch_size=32, validation_data = [testing_x,testing_y], epochs = numEpochs, callbacks=[acc_Callback], verbose = 0)                
+        info = model.fit(training_x, training_y, batch_size=32, validation_data = [testing_x,testing_y], epochs = numEpochs, callbacks=[acc_Callback], verbose = 0)
         #Make plot of training and testing error
         acc_matrix[idx,:] = np.asarray(acc_Callback.accuracies).astype(float)
         val_acc_matrix[idx,:] = np.asarray(acc_Callback.val_accuracies).astype(float)
         idx = idx + 1
-        
-    gen_acc = acc_matrix.mean(axis = 0)                   
+
+    gen_acc = acc_matrix.mean(axis = 0)
     gen_val_acc = val_acc_matrix.mean(axis = 0)
-                  
+
     epochs = np.arange(numEpochs) + 1
     plot.figure(1)
-    plot.plot(epochs, (1 - gen_acc), 'r') 
-    plot.plot(epochs, (1 - gen_val_acc), 'b') 
+    plot.plot(epochs, (1 - gen_acc), 'r')
+    plot.plot(epochs, (1 - gen_val_acc), 'b')
     plot.legend({'Training','Testing'},fontsize = 12)
     plot.xlabel('Epochs',fontsize = 16), plot.ylabel('Misclassification Rate',fontsize = 16)
     plot.xlim([1,numEpochs]), plot.ylim([0, (np.amax(1 - gen_val_acc) + 0.1)])
     plot.title('Generalized Error',fontsize = 20)
-    
+
 def ROC_and_PR_plots(X,y, model, numEpochs = 1000):
     earlyStop = callbacks.EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=100, mode='min', baseline=None, restore_best_weights=False)
 
@@ -65,7 +65,7 @@ def ROC_and_PR_plots(X,y, model, numEpochs = 1000):
     testing_ROC_AUC = []
     training_PR_AUC = []
     testing_PR_AUC = []
-    
+
     for train_index, test_index in kf.split(X = X, y = y):
         training_x = np.asarray(X)[train_index,:]
         testing_x = np.asarray(X)[test_index,:]
@@ -73,26 +73,26 @@ def ROC_and_PR_plots(X,y, model, numEpochs = 1000):
         testing_y = np.asarray(y)[test_index]
         val_data = [testing_x, testing_y]
         model.fit(training_x, training_y,batch_size=32, validation_data = val_data, epochs = numEpochs, verbose = 0,callbacks=[earlyStop])
-        
+
         #False/true positive rates for ROC
         training_y_pred = model.predict(training_x).ravel()
         testing_y_pred = model.predict(testing_x).ravel()
-    
+
         training_false_pos_rate, training_true_pos_rate, training_thresholds = roc_curve(training_y, training_y_pred)
         testing_false_pos_rate, testing_true_pos_rate, testing_thresholds = roc_curve(testing_y, testing_y_pred)
         training_ROC_AUC.append(roc_auc_score(training_y, training_y_pred))
         testing_ROC_AUC.append(roc_auc_score(testing_y, testing_y_pred))
-        
+
         #Precision and recall
         training_y_proba = model.predict_proba(training_x).ravel()
         testing_y_proba = model.predict_proba(testing_x).ravel()
-    
+
         training_precision, training_recall, training_thresholds_PR = precision_recall_curve(training_y, training_y_proba)
         testing_precision, testing_recall, training_thresholds_PR = precision_recall_curve(testing_y, testing_y_proba)
-    
+
         training_PR_AUC.append(auc(training_recall, training_precision))
         testing_PR_AUC.append(auc(testing_recall, testing_precision))
-    
+
         #plot labels
         labelTr = "_nolegend_"
         labelTe = "_nolegend_"
@@ -100,7 +100,7 @@ def ROC_and_PR_plots(X,y, model, numEpochs = 1000):
             labelTr = "Training"
             labelTe = "Testing"
         idx = idx + 1
-        
+
         #Precision-Recall Plot
         plot.figure(3)
         plot.plot(training_recall, training_precision, 'r', label = labelTr, linewidth = 0.4)
@@ -109,7 +109,7 @@ def ROC_and_PR_plots(X,y, model, numEpochs = 1000):
         plot.title('PR curve',fontsize = 20)
         plot.legend(fontsize = 12, loc='best')
         plot.xlim([0,1]), plot.ylim([0,1])
-    
+
         #ROC plot
         plot.figure(2)
         plot.plot([0,1],[0,1],'k--',label= '_nolegend_')
@@ -119,26 +119,26 @@ def ROC_and_PR_plots(X,y, model, numEpochs = 1000):
         plot.title('ROC curve',fontsize = 20)
         plot.legend(fontsize = 12, loc='best')
         plot.xlim([0,1]), plot.ylim([0,1])
-    
-    
+
+
     plot.figure(2)
     ROC_val_Tr = str(np.round(np.asarray(training_ROC_AUC).mean(0),3))
     AUC_text_Tr = 'Training AUC: ' + ROC_val_Tr
     plot.text(0.6, 0.45, AUC_text_Tr ,fontsize = 12)
-    
+
     ROC_val_Te = str(np.round(np.asarray(testing_ROC_AUC).mean(0),3))
     AUC_text_Te = 'Testing AUC: ' + ROC_val_Te
     plot.text(0.6, 0.35, AUC_text_Te ,fontsize = 12)
-                              
+
     plot.figure(3)
     PR_val_Tr = str(np.round(np.asarray(training_PR_AUC).mean(0),3))
     AUC_text_Tr = 'Training AUC: ' + PR_val_Tr
     plot.text(0.05, 0.45, AUC_text_Tr ,fontsize = 12)
-    
+
     PR_val_Te = str(np.round(np.asarray(testing_PR_AUC).mean(0),3))
     AUC_text_Te = 'Testing AUC: ' + PR_val_Te
     plot.text(0.05, 0.35, AUC_text_Te ,fontsize = 12)
-    
+
 
 def load_data():
     file = "spambase.data"
@@ -220,9 +220,9 @@ def construct_model(num_nodes, num_layers, learning_rate, loss_fn, activation_fn
         ann.add(Dense(units=num_nodes, activation=activation_fn)) #hidden layers
 
     ann.add(Dense(units=1, activation=output_fn)) #output layer
-        
+
     sgd = optimizers.SGD(lr=learning_rate, decay=1e-6, momentum=0.9, nesterov=False)
-           
+
     ann.compile(loss=loss_fn,
                 optimizer= sgd,
                 metrics=["binary_accuracy"])
@@ -240,7 +240,7 @@ def grid_search(training_data, testing_data, max_hidden_layers, nodes_per_layer,
     for layer in range(max_hidden_layers):
         for i, nodes in enumerate(nodes_per_layer):
             model = construct_model(nodes, layer+1, learning_rate, loss_fn, activation_fn, output_fn)
-            logs = model.fit(training_x, training_y, 
+            logs = model.fit(training_x, training_y,
                             validation_data=(testing_x, testing_y),
                             shuffle=True,
                             batch_size=32,
@@ -249,7 +249,7 @@ def grid_search(training_data, testing_data, max_hidden_layers, nodes_per_layer,
                             callbacks=[earlyStop])
             training_error[layer][i] = 1-logs.history['binary_accuracy'][-1]
             testing_error[layer][i] = 1-logs.history['val_binary_accuracy'][-1]
-    
+
     return (training_error, testing_error)
 
 def k_fold(data, test_index, k):
@@ -280,8 +280,25 @@ def main():
         print("\nTraining error for k=", i, ":\n", training_error)
         print("\nTesting error for k=", i, ":\n", testing_error)
         print("------------------------------------------------------\n")
-    
+
     print("\nGeneralized training errors:\n", training_errors.mean(0))
     print("\nGeneralized testing errors:\n", testing_errors.mean(0))
 
-main()
+def testing():
+    data = load_data()  #returns min-max scaled data
+    hidden_layers = 3
+    nodes_per_layer = 10
+    activation_fn = "sigmoid"
+    output_fn = "sigmoid"
+    loss_fn = "mean_squared_error"
+    lr = 0.005
+    k = 10
+
+    x = data[:, :-1]
+    y = data[:, -1]
+    model = construct_model(num_nodes, hidden_layers, lr, loss_fn, activation_fn, output_fn)
+    error_plot(x, y, model)
+    ROC_and_PR_plots(x, y, model)
+
+#main()
+testing()
